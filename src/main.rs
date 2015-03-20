c29991229fa8e69c3faf041d938f0a4551eaa120
Zahari Petkov <zahari@polydraw.com>
2015-03-20 18:06:44 +0200

    Printing out browser's user agent.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ extern crate iron;
 
 use iron::prelude::*;
 use iron::status;
+use iron::headers::UserAgent;
 
 fn main() {
    fn hello_world(request: &mut Request) -> IronResult<Response> {
-      let content = format!("{:?}", request);
+      let user_agent = request.headers.get::<UserAgent>().unwrap();
+      let content = format!("{:?}", user_agent);
       Ok(Response::with((status::Ok, content)))
    }
 
